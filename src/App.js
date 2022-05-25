65b41c1eaccc0de1446e52b25d3f77a89fe0d4ea
Baurens <48996314+Phildevfront@users.noreply.github.com>
2022-05-25 11:20:24 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './img/logo_msj_mac_colored.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <div className='container-logo'>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className='main-title'>SUPERJOLI</h1>
+        <h3>BEAUTIFUL COMMUNICATION</h3>
+      </div>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
