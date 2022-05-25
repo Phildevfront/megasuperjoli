8235e0970b056cb8f8206ec86c1583d945596903
Baurens <48996314+Phildevfront@users.noreply.github.com>
2022-05-25 16:50:57 +0200

    Add Header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,21 @@
-import logo from './img/logo_msj_mac_colored.png';
-import './App.css';
+import Header from './Header';
+import logo from '../img/logo_msj_mac_colored.png';
+import s from '../img/s_noir.png';
+import '../styles/App.css';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header>
       <div className='container-logo'>
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={s} className="s-logo" alt="logo" />
         <h1 className='main-title'>SUPERJOLI</h1>
         <h3 className='sub-title'>BEAUTIFUL COMMUNICATION</h3>
       </div>
-
-      </header>
+      </Header>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import '../styles/Header.css'
+
+function Header({ children }) {
+    return <div className='App-header'>{children}</div>
+}
+
+export default Header
